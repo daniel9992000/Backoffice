ebcd91525af5dff842e02ac9a445d05b1be2f158
MasterLindi <kridl.ovka1@gmx.at>
2012-03-13 10:21:46 +0100

    added IDAL and LocalDB

---
+++ Backoffice/LocalDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Npgsql;
+using System.Windows.Forms;
+
+namespace Backoffice
+{
+    class LocalDB
+    {
+        public void buildconnection ()
+        {
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server={0};Port={1};" +
+                    "User Id={2};Password={3};Database={4};",
+                    "localhost", 5432, "swe",
+                    "swe2", "postgres");
+                // Making connection with Npgsql provider
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // quite complex sql statement
+                string sql = "SELECT * FROM simple_table";
+                // data adapter making request from our connection
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                // i always reset DataSet before i do
+                // something with it.... i don't know why :-)
+                MessageBox.Show("hello");
+                conn.Close();
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+               
+               
+            }
+        }
+       
+    }
+}
